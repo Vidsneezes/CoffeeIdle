b5ca8875c31a451612c5ee74b9845135f1cf8c3d
Vidsneezes <moraleszez@gmail.com>
2017-04-15 16:03:18 -0600

    updates generator component

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { PropTypes } from 'react';
 
 const Generator = ({name, amount, nextCost}) => {
@@ -28,33 +27,3 @@ BoardDisplay.propTypes = {
 
 
 export default BoardDisplay;
-=======
-import React, { Component } from 'react';
-
-class Generator extends Component {
-  render(){
-    let buttonStatus = "";
-    if(this.props.disabled){
-      buttonStatus = "-dis";
-    }
-
-    return(
-      <section className="generator-container">
-        <div className="generatorcontent-right">
-          <button className="rectangle" onClick={()=>this.props.onClick()} disabled={this.props.disabled}>
-           Buy
-          </button>
-        </div>
-        <div className="generatorcontent-left">
-          <h5 className="center">Owned:{this.props.amount}</h5>
-          <p className="center">{this.props.text}</p>
-          <p className="center">{"costs: "+this.props.cost}</p>
-          <p className="center">{this.props.production+"$/s"}</p>
-        </div>
-      </section>
-    );
-  }
-}
-
-export default Generator;
->>>>>>> e0a1fe127b7e29936ae408f66f14d7d156ea312c
