76250bc86489223dfedf616d4904fafb37c7afbd
Vidsneezes <moraleszez@gmail.com>
2017-04-15 16:00:50 -0600

    added generator component, no logic

---
+++ src/components/Generator.jsx
@@ -0,0 +1,29 @@
+import React, { PropTypes } from 'react';
+
+const Generator = ({name, amount, nextCost}) => {
+    /* if(done === false)
+     {
+        gridPieces = grid.map((val,index) => 
+            <InteractionBox key={index} indexPosition={index} />)
+     }else{
+         gridPieces = (
+             <p>
+             Game done
+             </p>
+         )
+     }
+     
+     return (
+        <div>
+            {gridPieces}
+        </div>
+     )*/
+}
+
+BoardDisplay.propTypes = {
+    grid: PropTypes.array,
+    done: PropTypes.bool
+}
+
+
+export default BoardDisplay;
