75896ceca9a63e08661c9eada9ebaa5dcbccbcc0
Vidsneezes <moraleszez@gmail.com>
2017-04-17 19:38:22 -0600

    implemented reducer for change buy amount

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
 import { List, Map } from 'immutable';
-import { INCREASE_CLICK, BUY_GENERATOR, INCREASE_TICK, initialState } from './actions';
+import { INCREASE_CLICK, BUY_GENERATOR, CHANGE_BUY_AMOUNT, INCREASE_TICK, initialState } from './actions';
 
 export function reducers(state = initialState , action){
     switch(action.type){
@@ -7,7 +7,7 @@ export function reducers(state = initialState , action){
             let cash = state.get('cash');
             cash = cash + state.get('cashPerClick');
             state.set('cash', cash);
-        return state;
+            return state;
         case BUY_GENERATOR:
             const amountToBuy = state.get('buyAmount');
             const generatorIndex = state.get('generators').findIndex(generator => {
@@ -16,7 +16,18 @@ export function reducers(state = initialState , action){
             let amount = state.getIn(['generators', generatorIndex, 'amount']) + amountToBuy;
             state.setIn(['generators',generatorIndex,'amount'],amount);
             return state;
-        return state;
+        case CHANGE_BUY_AMOUNT: 
+            const current = state.get('buyAmount');
+            let newAmount = 1;
+            if(current === 1){
+                newAmount = 5;
+            }else if(current === 5){
+                newAmount = 10;
+            }
+            state.set('buyAmount',newAmount);
+            return state;
+        default:
+            return state;
     }
 
     return state;
