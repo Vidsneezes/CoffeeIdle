1769f8ca1134659605728f0a54fa2ecbf7475bad
Vidsneezes <moraleszez@gmail.com>
2017-04-17 19:05:20 -0600

    added increase click action to actions, move intiial state from store to action index

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,21 @@
-export const addBase = (amount) => ({
-    type: 'ADD_BASE',
-    amount
-});
+import { List, Map } from 'immutable';
+
+export const INCREASE_CLICK = "INCREASE_CLICK";
+
+export const initialState = Map({
+    cash:0,
+    generators: List([
+        Map({
+            name:"Coffee Machine",
+            baseCost:6,
+            rate:1.07,
+            productionBase:3,
+            amount:0,
+            initialCost:0
+        })
+    ])
+})
+
+export const increaseClick = () => ({
+    type: INCREASE_CLICK
+})
