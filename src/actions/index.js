7193d69e0acc295f7e79b39ca3e3408c91e13799
Vidsneezes <moraleszez@gmail.com>
2017-04-17 19:26:25 -0600

    adds buy generator case to reducers, adds new exports to actions index

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,14 @@
 import { List, Map } from 'immutable';
 
 export const INCREASE_CLICK = "INCREASE_CLICK";
+export const BUY_GENERATOR = "BUY_GENERATOR";
+export const INCREASE_TICK = "INCREASE_TICK";
+export const CHANGE_BUY_AMOUNT = "CHANGE_BUY_AMOUNT"; 
 
 export const initialState = Map({
     cash:0,
+    buyAmount: 1,
+    cashPerClick: 1,
     generators: List([
         Map({
             name:"Coffee Machine",
@@ -16,6 +21,19 @@ export const initialState = Map({
     ])
 })
 
+export const changeBuyAmount = () => ({
+    type: CHANGE_BUY_AMOUNT
+})
+
 export const increaseClick = () => ({
     type: INCREASE_CLICK
+})
+
+export const buyGenerator = (generatorName) => ({
+    type: BUY_GENERATOR,
+    generatorName
+})
+
+export const increaseTick = () => ({
+    type: INCREASE_TICK
 })
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,15 @@
 import { List, Map } from 'immutable';
-import { INCREASE_CLICK, initialState } from './actions';
-
-let cashPerClick = 1;
+import { INCREASE_CLICK, BUY_GENERATOR, INCREASE_TICK, initialState } from './actions';
 
 export function reducers(state = initialState , action){
     switch(action.type){
         case INCREASE_CLICK:
             let cash = state.get('cash');
-            cash = cash + cashPerClick;
+            cash = cash + state.get('cashPerClick');
             state.set('cash', cash);
+        return state;
+        case BUY_GENERATOR:
+
         return state;
     }
 
