1136c63a4a0488a53d9745d22454611f7cc07550
Vidsneezes <moraleszez@gmail.com>
2017-04-17 19:15:04 -0600

    added state for increasing click count

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,16 @@
-export function reducers(state, action){
+import { List, Map } from 'immutable';
+import { INCREASE_CLICK, initialState } from './actions';
+
+let cashPerClick = 1;
+
+export function reducers(state = initialState , action){
+    switch(action.type){
+        case INCREASE_CLICK:
+            let cash = state.get('cash');
+            cash = cash + cashPerClick;
+            state.set('cash', cash);
+        return state;
+    }
+
     return state;
 }
