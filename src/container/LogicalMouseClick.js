ee0cb996196b62d5deae32fe6773d12070bbe305
Vidsneezes <moraleszez@gmail.com>
2017-04-21 11:00:33 -0600

    connected clickerarea to logicalmouseclick

--- src/container/LogicalMouseClick.js
+++ src/container/LogicalMouseClick.js
@@ -2,18 +2,19 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Map } from 'immutable';
 import ClickerArea from '../components/ClickerArea';
+import { increaseClick } from '../actions/index';
 
 const mapStateToProps = (state, ownProps) => {
     return {
-        grid: state.get('grid').toJS(),
-        done: state.get('done')
+        totalCash: state.get('grid').toJS(),
+        cashPreClick: state.get('done')
     }
 }
 
 const mapDispatchToProps = (dispatch, ownProps) => {
     return {
         onClick: () => {
-            dispatch()
+            dispatch(increaseClick)
         }
     }
 }
