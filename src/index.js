9bddd627fc144a64485465ea82c77fd34b7748af
Vidsneezes <moraleszez@gmail.com>
2017-04-21 11:03:53 -0600

    warpped app in provider in index js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import reducers from './reducers/index';
 import App from './components/App';
 import './index.css';
@@ -22,6 +23,8 @@ store.dispatch(increaseTick());
 unsubscribe();
 
 ReactDOM.render(
-  <App />,
+  <Provider store = {store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
