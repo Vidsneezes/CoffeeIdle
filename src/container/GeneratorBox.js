6d431ec2e88456edc91faa3b293d4c56f675b0bd
Vidsneezes <moraleszez@gmail.com>
2017-04-24 13:25:27 -0600

    added correct display to generator box

--- src/container/GeneratorBox.js
+++ src/container/GeneratorBox.js
@@ -3,13 +3,18 @@ import { connect } from 'react-redux';
 import { Map } from 'immutable';
 import Generator from '../components/Generator';
 import { buyGenerator } from '../actions/index';
+import {calculateCost, calculateProduction }from '../reducers/index.js';
 
 const mapStateToProps = (state, ownProps) => {
+    const baseCost = state.getIn(['generators',ownProps.indexN,'baseCost']);
+    const rate = state.getIn(['generators',ownProps.indexN,'rate']);
+    const amount = state.getIn(['generators',ownProps.indexN,'amount']);
+    const productionBase = state.getIn(['generators',ownProps.indexN,'productionBase']);
     return {
         name: state.get('generators').get(ownProps.indexN).get("name"),
-        amount:state.get('generators').get(ownProps.indexN).get("amount"),
-        producing:state.get('generators').get(ownProps.indexN).get("productionBase"),
-        nextCost:state.get('generators').get(ownProps.indexN).get("initialCost")
+        amount:amount,
+        producing: calculateProduction(productionBase,amount),
+        nextCost: calculateCost(baseCost,rate, amount)
     }
 }
 
